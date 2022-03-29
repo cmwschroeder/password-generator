087c341bc810ac54b3b0dc36c06184604b275359
cmwschroeder <cmwschroeder@gmail.com>
2022-03-29 09:56:54 -0700

    changed returning password from an array, added arrays for all possible characters to use in password

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var passLength = prompt("Enter how long you want your password to be between 8 and 128", "");
+  var newPass = ["a"];
+
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var spec = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|",
+   "}", "~"];
+
 
   if(passLength == null || passLength == "") {
     alert("Nothing was entered")
@@ -61,5 +69,5 @@ function generatePassword() {
   }
 
 
-  return "newPassword";
+  return newPass.join("");
 }
